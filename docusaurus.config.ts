23fd7866d31836c1ba1dfa633b040a3a56657d84
Niko <39569270+nikomancy@users.noreply.github.com>
2024-02-15 10:02:59 -0700

    Adding rest API Reference page
    Removing no longer needed files for API reference demo.

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -3,8 +3,8 @@ import type {Config} from '@docusaurus/types';
 import type * as Preset from '@docusaurus/preset-classic';
 
 const config: Config = {
-  title: 'My Site',
-  tagline: 'Dinosaurs are cool',
+  title: 'Example Developer Portal',
+  tagline: 'What a Dev Portal can do for you.',
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
@@ -15,8 +15,8 @@ const config: Config = {
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
+  organizationName: 'Niko Berry', // Usually your GitHub org/user name.
+  projectName: 'Example Dev Portal', // Usually your repo name.
 
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
@@ -58,7 +58,7 @@ const config: Config = {
     // Replace with your project's social card
     image: 'img/docusaurus-social-card.jpg',
     navbar: {
-      title: 'My Site',
+      title: 'Dev Portal',
       logo: {
         alt: 'My Site Logo',
         src: 'img/logo.svg',
@@ -68,7 +68,12 @@ const config: Config = {
           type: 'docSidebar',
           sidebarId: 'tutorialSidebar',
           position: 'left',
-          label: 'Tutorial',
+          label: 'Docs',
+        },
+        {
+          to: '/api-reference',
+          label: 'API Reference', 
+          position: 'left'
         },
         {to: '/blog', label: 'Blog', position: 'left'},
         {
---
+++ src/pages/api-reference.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import { RedocStandalone } from 'redoc';
+import Layout from '@theme/Layout';
+
+function ApiReference() {
+  return (
+    <Layout 
+      title="API Reference"
+      description='API Reference for the Game Score API.'
+    >
+      <RedocStandalone
+        specUrl="../game-score-api.yaml"
+        options={{
+          nativeScrollbars: true,
+          theme: { colors: { primary: { main: '#dd5522' } } },
+        }}
+      />
+    </Layout>
+  );
+}
+
+export default ApiReference;
